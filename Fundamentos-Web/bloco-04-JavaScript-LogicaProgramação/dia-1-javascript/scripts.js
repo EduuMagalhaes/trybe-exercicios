12f642ea514d725bc681ac09100a6019105d0b4a
Eduardo <dudu-system@hotmail.com>
2021-11-28 17:18:18 -0300

    segunda parte exercicios, Tipagem Dinamica e operaçoes aritmeticas

--- Fundamentos-Web/bloco-04-JavaScript-LogicaProgramação/dia-1-javascript/scripts.js
+++ Fundamentos-Web/bloco-04-JavaScript-LogicaProgramação/dia-1-javascript/scripts.js
@@ -5,3 +5,39 @@ console.log(myName, birthCity, birthYear);
 
 birthYear = 2030;
 console.log(birthYear);
+
+birthCity = "São Paulo";
+console.log(birthCity);
+ 
+
+
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
+
+patientId = "50";
+console.log(typeof patientId);
+
+
+const base = 5;
+let height = 8;
+const area = base * height;
+console.log("Base:",base," Height:",height," Area:",area);
+const perimeter = base*2 + height*2;
+console.log("Perimeter:",perimeter);
+
+
+
+
+ 
