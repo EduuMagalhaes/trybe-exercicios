945265f0aaa3ea889e1389acf18ccf977b801921
Eduardo <dudu-system@hotmail.com>
2021-11-28 18:27:37 -0300

    terceira parte do exercicio, Condições If e Else

--- Fundamentos-Web/bloco-04-JavaScript-LogicaProgramação/dia-1-javascript/scripts.js
+++ Fundamentos-Web/bloco-04-JavaScript-LogicaProgramação/dia-1-javascript/scripts.js
@@ -1,3 +1,5 @@
+/* Primeira parte */
+
 const myName = "Eduardo";
 const birthCity = "Uberlandia";
 let birthYear = 1992;
@@ -6,11 +8,10 @@ console.log(myName, birthCity, birthYear);
 birthYear = 2030;
 console.log(birthYear);
 
-birthCity = "São Paulo";
-console.log(birthCity);
- 
+/* birthCity = "São Paulo";
+console.log(birthCity); */
 
-
+/* Segunda parte */
 
 let patientId = 50;
 let isEnrolled = true;
@@ -37,7 +38,17 @@ console.log("Base:",base," Height:",height," Area:",area);
 const perimeter = base*2 + height*2;
 console.log("Perimeter:",perimeter);
 
+/* Terceira parte */
 
-
+const nota = 15;
+if(nota >= 80){
+  console.log("Parabéns, você foi aprovada(o)!");
+}
+else if(nota < 80 && nota >= 60){
+  console.log("Você está na nossa lista de espera");
+}
+else{
+  console.log("Você foi reprovada(o)");
+}
 
  
