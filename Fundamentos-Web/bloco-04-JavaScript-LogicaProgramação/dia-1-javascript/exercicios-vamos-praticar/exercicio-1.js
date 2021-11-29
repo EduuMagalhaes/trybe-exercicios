4eb2f8650371850e59d2d57be02d297cd93a6ab5
Eduardo <dudu-system@hotmail.com>
2021-11-28 21:40:48 -0300

    resoluçao exercicio 1

---
+++ Fundamentos-Web/bloco-04-JavaScript-LogicaProgramação/dia-1-javascript/exercicios-vamos-praticar/exercicio-1.js
@@ -0,0 +1,17 @@
+const a = 10;
+const b = 3;
+
+/* Adição (a + b) */
+console.log(a+b);
+
+/* Subtração (a - b) */
+console.log(a-b);
+
+/* Multiplicação (a * b) */
+console.log(a*b);
+
+/* Divisão (a / b) */
+console.log(a/b);
+
+/* Módulo (a % b) */
+console.log(a%b);
