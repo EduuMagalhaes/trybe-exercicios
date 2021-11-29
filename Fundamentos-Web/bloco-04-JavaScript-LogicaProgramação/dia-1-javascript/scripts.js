95b4dd51a3cdaaf90836d29a5353b6aed11d293d
Eduardo <dudu-system@hotmail.com>
2021-11-28 21:07:08 -0300

    quarta parte do exercicio, Operadores logicos

--- Fundamentos-Web/bloco-04-JavaScript-LogicaProgramação/dia-1-javascript/scripts.js
+++ Fundamentos-Web/bloco-04-JavaScript-LogicaProgramação/dia-1-javascript/scripts.js
@@ -51,4 +51,54 @@ else{
   console.log("Você foi reprovada(o)");
 }
 
+/* Quarta parte */
+
+const currentHour = 5;
+let message;
+
+if(currentHour>=22){
+  message = "Não deveríamos comer nada, é hora de dormir";
+}
+if(currentHour>=18 && currentHour<22){
+  message = "Rango da noite, vamos jantar :D";
+}
+if(currentHour>=14 && currentHour<18){
+  message = "Vamos fazer um bolo pro café da tarde?";
+}
+if(currentHour>=11 && currentHour<14){
+  message = "Hora do almoço!!!";
+}
+if(currentHour>=4 && currentHour<11){
+  message = "Hmmm, cheiro de café recém passado";
+}
+console.log(message);
+
+
+
+let weekDay = "quarta-feira"
+
+if(weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira"){
+  console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+}else if(weekDay === "sabado" || weekDay === "domingo"){
+  console.log("FINALMENTE, descanso merecido UwU");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  
