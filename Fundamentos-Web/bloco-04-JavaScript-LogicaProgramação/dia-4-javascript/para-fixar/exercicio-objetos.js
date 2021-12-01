a782aa0fcaabf4e95272324e31f15a6e7e0edc38
Eduardo <dudu-system@hotmail.com>
2021-11-30 21:53:13 -0300

    resoluçao do exercicio 3 de objetos

--- Fundamentos-Web/bloco-04-JavaScript-LogicaProgramação/dia-4-javascript/para-fixar/exercicio-objetos.js
+++ Fundamentos-Web/bloco-04-JavaScript-LogicaProgramação/dia-4-javascript/para-fixar/exercicio-objetos.js
@@ -5,3 +5,5 @@ let player = {
   medals : { golden: 2, silver: 3 }
 }
 console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade.");
+
+player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
