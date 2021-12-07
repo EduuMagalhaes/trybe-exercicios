9c44c295e54b44e5483202c9e6df0ba2a1f2b30b
Eduardo <dudu-system@hotmail.com>
2021-12-07 02:10:10 -0300

    exercicio 01

---
+++ Fundamentos-Web/bloco-05-JavaScript-DOM-Eventos-WebStorage/dia-2-javaScript-elementos/scripts.js
@@ -0,0 +1,6 @@
+let body = document.querySelector('body');
+
+// Exercicio 01
+let tagH1 = document.createElement('h1');
+tagH1.innerText = 'Exercício 5.2 - JavaScript DOM';
+body.appendChild(tagH1);
